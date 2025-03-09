217652c7def25df059ae1c6fcc86fe07b110ac38
safwanahmadsaffi <sahmedsaffi@gmail.com>
2025-03-09 12:58:36 +0500

    adding matrix sequentinal function

--- Sequential Implementation.c
+++ Sequential Implementation.c
@@ -1,3 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#define N 500 
+
+void matrixComputation(int A[N][N], int B[N][N], int C[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
